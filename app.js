49a7d6a30b994dc339abcdcf5d657257db049559
Chris Castle <chrisjcastle93@gmail.com>
2021-12-17 09:01:18 +0000

    Jest Testing from Lecture

--- app.js
+++ app.js
@@ -9,66 +9,66 @@ let playerTurn = 0;
 */
 
 class Player {
-    constructor(turn, name) {
-        // we are checking if the parameters have the right types
-        if (typeof turn !== 'number' || typeof name !== 'string') {
-            console.error('Wrong parameters types. Expect a number and a string')
-            return null;
-        }
-        this.name = name;
-        this.turn = turn;
-        this.health = 100;
-        this.power = 5;
+  constructor(turn, name) {
+    // we are checking if the parameters have the right types
+    if (typeof turn !== "number" || typeof name !== "string") {
+      console.error("Wrong parameters types. Expect a number and a string");
+      return null;
     }
-    // takeTurn makes sure the player can play at this time before making a move
-    takeTurn(cb) {
-      if (playerTurn !== this.turn) {
-          console.error(`Not your turn ${this.name}!!`);
-      } else {
-          cb();
-          playerTurn = (playerTurn + 1) % 2;
+    this.name = name;
+    this.turn = turn;
+    this.health = 100;
+    this.power = 5;
+  }
+  // takeTurn makes sure the player can play at this time before making a move
+  takeTurn(cb) {
+    if (playerTurn !== this.turn) {
+      console.error(`Not your turn ${this.name}!!`);
+    } else {
+      cb();
+      playerTurn = (playerTurn + 1) % 2;
+    }
+  }
+  // attack an other player
+  attack(enemy) {
+    if (!enemy) {
+      return `This enemy doesn't exist...`;
+    }
+    this.takeTurn(() => {
+      enemy.health -= this.power;
+      if (enemy.health <= 0) {
+        return `GAME OVER ${enemy.name.toUpperCase()}!! Congrats ${this.name})`;
       }
-    }
-    // attack an other player
-    attack(enemy) {
-        if (!enemy) {
-            return `This enemy doesn't exist...`;
-        }
-        this.takeTurn(() => {
-          enemy.health -= this.power;
-          if (enemy.health <= 0) {
-              return `GAME OVER ${enemy.name.toUpperCase()}!! Congrats ${this.name} ;)`
-          }
-        })
-    }
-    // regain health, then pass turn
-    help() {
-        this.takeTurn(() => {
-          this.health = Math.min(100, this.health + 3);
-        })
-    }
+    });
+  }
+  // regain health, then pass turn
+  help() {
+    this.takeTurn(() => {
+      this.health = Math.min(100, this.health + 3);
+    });
+  }
 }
 /*
     the function giveScore is just returning the current score
 */
 const giveScore = (playerA, playerB) => {
-    if (!playerA || !playerB || !playerA.name || !playerB.name) {
-        throw new Error('Undefined players')
-    }
-    if (playerA.name === playerB.name) {
-        throw new Error('Those are the same players !')
-    }
-    return `${playerA.name}:${playerA.health} - ${playerB.name}:${playerB.health}`
-}
+  if (!playerA || !playerB || !playerA.name || !playerB.name) {
+    throw new Error("Undefined players");
+  }
+  if (playerA.name === playerB.name) {
+    throw new Error("Those are the same players !");
+  }
+  return `${playerA.name}:${playerA.health} - ${playerB.name}:${playerB.health}`;
+};
 /*
     create 2 new players
 */
-const player1 = new Player(0, 'Daniel');
-const player2 = new Player(1, 'Jonas');
+const player1 = new Player(0, "Daniel");
+const player2 = new Player(1, "Jonas");
 
 /*
     Export the players and function to be tested in spec/app.spec.js
 */
-if (typeof module !== 'undefined') {
-    module.exports = {player1, player2, giveScore}
+if (typeof module !== "undefined") {
+  module.exports = { player1, player2, giveScore };
 }
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,37 +1,88 @@
 // imports use the syntax const foo = require('./path/to/fileName')
-const { player1: daniel, player2: jonas, giveScore } = require('../app');
+const { player1: daniel, player2: jonas, giveScore } = require("../app");
 
+describe("The Player class", () => {
+  it("should be a class", () => {
+    expect(typeof daniel).toBe("object");
+    expect(typeof jonas).toBe("object");
+  });
 
-describe('The Player class', () => {
-
-    it('should be a class', () => {
-        expect(typeof daniel).toBe("object");
-        expect(typeof jonas).toBe("object");
-    });
-
-    it('should have health set to 100', () => {
-        expect(daniel.health).toEqual(100);
-    });
+  it("should have health set to 100", () => {
+    expect(daniel.health).toEqual(100);
+  });
 });
 
-describe('The giveScore function', () => {
+describe("The giveScore function", () => {
+  it("should be a function", () => {
+    expect(typeof giveScore).toBe("function");
+  });
+});
 
-    it('should be a function', () => {
-        expect(typeof giveScore).toBe("function");
-    });
+describe("The player's properties", () => {
+  it("should be an object", () => {
+    expect(daniel.name).toBe("Daniel");
+    expect(daniel.turn).toBe(0);
+    expect(daniel.power).toBe(5);
+    expect(daniel.health).toBe(100);
+  });
+});
+
+describe("The player's attack", () => {
+  it("should be a function", () => {
+    expect(typeof daniel.attack).toBe("function");
+    expect(typeof jonas.attack).toBe("function");
+  });
+  it("should behave as expected", () => {
+    expect(jonas.health).toBe(100);
+    daniel.attack(jonas);
+    expect(jonas.health).toBe(95);
+  });
+  it("should handle exceptions and errors", () => {
+    expect(jonas.attack()).toBe("This enemy doesn't exist...");
+  });
+});
+
+describe("The player's health", () => {
+  it("should recover when help function is used", () => {
+    daniel.attack(jonas);
+    jonas.help();
+    expect(jonas.health).toBe(98);
+  });
+  it("should never be above 100", () => {
+    jonas.health = 100;
+    jonas.help();
+    expect(jonas.health).toBe(100);
+  });
+});
+
+describe("The giveScore function", () => {
+  it("should throw an error with a player missing", () => {
+    expect(() => {
+      giveScore();
+    }).toThrow("Undefined players");
+  });
+  it("should throw an error with same player names", () => {
+    expect((name1, name2) => {
+      giveScore(daniel, daniel);
+    }).toThrow(new Error("Those are the same players !"));
+  });
+});
+
+describe("Check that game over is working correctly", () => {
+  it("should say GAME OVER when health drops below 0", () => {
+    jonas.health = 3;
+    daniel.attack(jonas)
+    // expect( enemy => {
+    //   daniel.attack(jonas);
+    // }).toBe("GAME OVER JONAS!! Congrats Daniel");
+    // const result = daniel.attack(jonas)
+    console.log(jonas)
+    // console.log(daniel.attack(jonas))
+    // console.log(result)
+    // expect(result).toBe("GAME OVER JONAS!! Congrats Daniel");
+  });
 });
 
 /* TODO :
-    check player's properties
-    
-    check player's attack function
-        is it a function ?
-        is it behaving as expected ?
-        are exceptions and error well handled ?
-    
-    check player's help function
-    
-    check the giveScore function
-
     check when the game ends
 */
